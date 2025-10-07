6711431720bc81c3ec353e8221f56612ec312e68
杨伟杰 <justlikesuolong@outlook.com>
2025-10-08 05:30:42 +0800

    feat: 默认文档是暗色模式

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   description: 'Vite + NestJS 全栈方案',
   lang: 'zh-CN',
 
+  // 设置默认为暗模式
+  appearance: 'dark',
+
   // 忽略本地开发服务器的死链接
   ignoreDeadLinks: [
     /^http:\/\/localhost:\d+/,
